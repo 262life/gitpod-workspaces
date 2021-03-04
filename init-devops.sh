2de4651c9570e9554a8aa7588e1c29398d4d0402
Robert Liotta <bob@bobdot.me>
2021-03-04 01:27:22 +0000

    New Feature

--- init-devops.sh
+++ init-devops.sh
@@ -1,9 +1,11 @@
-#!/bin/sh
+#!/bin/bash
+
+source .github/scripts/build_functions.sh
 
 export docker_login=${DOCKER_LOGIN}
 
 export docker_token=${DOCKER_TOKEN}
 
-LATEST=false TAG=v1.0 PUBLISH=true DOCKERFILE=./Dockerfile-devops .github/scripts/init.sh gitpod-workspace-devops
+LATEST=false TAG=v1.1 PUBLISH=true DOCKERFILE=./Dockerfile-devops .github/scripts/init.sh gitpod-workspace-devops
 
 
