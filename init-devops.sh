cfa89392bdf593974b6514a7a4fdf40c3f48f544
Robert Liotta <bob@bobdot.me>
2021-03-04 01:28:02 +0000

    Update.

--- init-devops.sh
+++ init-devops.sh
@@ -6,6 +6,6 @@ export docker_login=${DOCKER_LOGIN}
 
 export docker_token=${DOCKER_TOKEN}
 
-LATEST=false TAG=v1.1 PUBLISH=true DOCKERFILE=./Dockerfile-devops .github/scripts/init.sh gitpod-workspace-devops
+LATEST=true TAG=v1.1 PUBLISH=true DOCKERFILE=./Dockerfile-devops .github/scripts/init.sh gitpod-workspace-devops
 
 
